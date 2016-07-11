aab1d88ce479de386029b0248c42fcb45ecd51eb
Marthyn <Marthyn@live.nl>
2016-07-11 20:21:02 +0200

    Create spec for checking the rover's positions

--- spec/mission_spec.rb
+++ spec/mission_spec.rb
@@ -11,15 +11,21 @@ describe Mission do
       "MMRMMRMRRM"
     ]
   }
+
+  let(:exomars) { Mission.new(lines) }
+
   it "can initialize a mission with input" do
     expect(Mission.new(sample_input)).to be_a(Mission)
   end
 
   it "can initialize the planet with a bounded area based on given input" do
-    mission = Mission.new(lines)
-    expect(mission.planet.bounds.position).to eq([5, 5])
+    expect(exomars.planet.bounds.position).to eq([5, 5])
   end
 
   it "sets up the rovers with initial positions" do
+    expect(exomars.rovers[0].position).to eq([1, 2])
+    expect(exomars.rovers[1].position).to eq([3, 3])
+    expect(exomars.rovers[0].heading.to_s).to eq("N")
+    expect(exomars.rovers[1].heading.to_s).to eq("E")
   end
 end
