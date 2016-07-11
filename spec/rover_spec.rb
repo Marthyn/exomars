72b68dbdc8afebc8ffff128ca8af3a69f0d55f8f
Marthyn <Marthyn@live.nl>
2016-07-11 19:01:52 +0200

    Create some basic specs for the Rovers initialization and movement

--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -1,5 +1,28 @@
 require 'mission'
 
 describe Rover do
+  let(:schiaparelli) { Rover.new }
 
+  it "has a default position" do
+    expect(schiaparelli.position).to_eq([0, 0])
+  end
+
+  it "can move around Mars and return a new position" do
+    schiaparelli.move
+    expect(schiaparelli.position).to eq([0, 1])
+  end
+
+  it "has a default heading" do
+    expect(schiaparelli.heading).to eq('N')
+  end
+
+  it "has the correct heading after turning right" do
+    schiaparelli.turn('R')
+    expect(schiaparelli.heading).to eq('E')
+  end
+
+  it "has the correct heading after turning left" do
+    schiaparelli.turn('R')
+    expect(schiaparelli.heading).to eq('W')
+  end
 end
