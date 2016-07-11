56eaa2f29ccc1498f23291bf104bb87da62b3d8e
Marthyn <Marthyn@live.nl>
2016-07-11 21:24:07 +0200

    Add mission simulation

--- spec/mission_spec.rb
+++ spec/mission_spec.rb
@@ -29,8 +29,14 @@ describe Mission do
     expect(exomars.rovers[1].heading.to_s).to eq("E")
   end
 
-  it "returns the expected output" do
+  it "simulates the mission" do
+    puts "[EXOMARS] Sending rovers to Mars"
+    puts "[EXOMARS] Mars' area of exploration is #{exomars.planet.bounds.x} by #{exomars.planet.bounds.y} clicks"
+    puts "[EXOMARS] Rovers landed, initiating exploration"
     exomars.start
+    puts "[EXOMARS] Great success! The final positions are:"
+    puts "[EXOMARS] Rover 1: #{exomars.output[0]}"
+    puts "[EXOMARS] Rover 2: #{exomars.output[1]}"
     expect(exomars.output[0]).to eq("1 3 N")
     expect(exomars.output[1]).to eq("5 1 E")
   end
